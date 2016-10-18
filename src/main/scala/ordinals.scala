ec8c30fb7a87294b141a32c12cb83237eeae7bf0
Nikolaus Thiel <nikolaus.thiel@leonteq.com>
2016-10-18 18:19:14 +0200

    Ordinals go parallel
    - ParVector instead of List
    
    Also better logging by using new util package

--- src/main/scala/ordinals.scala
+++ src/main/scala/ordinals.scala
@@ -1,99 +1,123 @@
 package ordinals
 
 import scala.annotation.tailrec
-
+import scala.collection.immutable.Vector
+import scala.collection.parallel.immutable._
+import Ordinal._
 
 sealed trait Ordinal {
+
+  final def getExpList: ParVector[Ordinal] = this match {case Ord(ords) => ords}
 
   final override def toString: String = {
     // a beautiful string
     if (this.isFinite)
       this.toBigInt.get.toString
-    else this match {
-      case Ord(ords) => Ordinal.ordListToString(ords)
-    }
+    else Ordinal.ordVecToString(this.getExpList)
   }
 
-  final def isZero: Boolean = this match {
-      // returns true iff this is zero
-    case Ord(List()) => true
-    case Ord(ords) => false
-  }
+  final def isZero: Boolean = (this == zero)
 
-  final def isFinite: Boolean = this match {
-    // returns true iff this is finite
-    case Ord(ords) => ords.forall(_.isZero)
-  }
+  final def isFinite: Boolean = this.getExpList.forall(_.isZero)
 
-  final def toBigInt: Option[BigInt] = {
-    // returns Some(i) if this is the finite ordinal i
-    // None otherwise
-    if (this.isFinite)
-      this match {
-        case Ord(ords) => Option(ords.length)
-      }
-    else None
-  }
+  final def toBigInt: Option[BigInt] = if (this.isFinite) Some(this.getExpList.length) else None
+  // toBigInt returns Some(i) iff this is the finite ordinal i
 
-  final def isSmaller(beta: Ordinal): Boolean = this match {
+  final def isSmaller(beta: Ordinal): Boolean = {
     // returns true iff this is smaller than that
-    case Ord(List()) => !(beta == Ordinal.zero)
-    case Ord(List(alphas)) => true
+    if (this.isFinite)
+      if (beta.isFinite)
+        this.toBigInt.get < beta.toBigInt.get
+      else true
+    // work in progress !!!    
+      else {
+        println("isSmaller:  !!!! WORK IN PROGRESS: Do not trust the result !!!!")
+        if (beta.isFinite) false else true
+        }
   }
-
 }
 
-//case object Nil extends Ordinal
-case class Ord(ords: List[Ordinal]) extends Ordinal
-
+case class Ord(ords: ParVector[Ordinal]) extends Ordinal
 
 object Ordinal {
-  final val zero: Ordinal = Ord(List())
-  final val one: Ordinal = Ord(List(zero))
-  final val omega: Ordinal = Ord(List(one))
+  final val zero: Ordinal = Ord(Vector().par)
+  final val one: Ordinal = Ord(Vector(zero).par)
+  final val omega: Ordinal = Ord(Vector(one).par)
   final val omegaStr: String = "W"
 
+  final def apply(i: Int): Ordinal = Ord(Vector.fill(i.max(0))(zero).par)
 
-  final def ordListToString(ords: List[Ordinal]): String = ords match {
-    case List() => "NIL"
-    case o :: List() => omegaStr + "^(" + o + ")"
-    case o :: os => omegaStr + "^(" + o + ") + " + ordListToString(os)
+  final def ordVecToString(ords: ParVector[Ordinal]): String = {
+    @tailrec
+    def go(ords: ParVector[Ordinal], str: String): String = {
+      if (ords.isEmpty) str
+      else {
+        val ordsHead: Ordinal = ords.head
+        val headExponent: String = {
+          if (ordsHead == one) ""
+          else {
+            val ordsHeadStr: String = ordsHead.toString
+            if (ordsHeadStr.length == 1) "^" + ordsHeadStr else "^(" + ordsHeadStr + ")"
+          }
+        }
+        if (ords.length == 1) str + omegaStr + headExponent
+        else go(ords.tail, str + omegaStr + headExponent)
+      }
+    }
+    go(ords, "")
   }
 
-  final def apply(i: Int):Ordinal = Ord(List().padTo(i.max(0),zero))
+  final def omegaStack(n: BigInt): Ordinal = {
+    @tailrec
+    def go(i: BigInt, alpha: Ordinal): Ordinal = {
+      if (i < 1) alpha
+      else go(i - 1, Ord(Vector(alpha).par))
+    }
+    go(n, one)
+  }
 
 }
-
 
 object ordinalApp extends App {
 
+  import util._
+  logg("System.getProperty(\"file.encoding\")")(System.getProperty("file.encoding"))
+  logg("Hellenic letters")("α β γ ω")
   println("****************************")
   println("********* Ordinals *********")
   println("****************************")
-  println("System.getProperty(\"file.encoding\") = " + System.getProperty("file.encoding"))
-  println(s"Hellenic letters : α β γ ω")
+  println("zero                  = " + zero)
+  println("zero.isZero           = " + zero.isZero)
+  println("zero.isFinite         = " + zero.isFinite)
+  println("Ordinal(1)            = " + Ordinal(1))
+  println("Ordinal(1).isZero     = " + Ordinal(1).isZero)
+  println("Ordinal(1).isFinite   = " + Ordinal(1).isFinite)
+  println("Ordinal(42)           = " + Ordinal(42))
+  println("Ordinal(42).isZero    = " + Ordinal(42).isZero)
+  println("Ordinal(42).isFinite  = " + Ordinal(42).isFinite)
+  println("Ordinal(-42)          = " + Ordinal(-42))
+  println("Ordinal(-42).isZero   = " + Ordinal(-42).isZero)
+  println("Ordinal(-42).isFinite = " + Ordinal(-42).isFinite)
+  println("omega                 = " + omega)
+  println("omega.isZero          = " + omega.isZero)
+  println("omega.isFinite        = " + omega.isFinite)
+  println("omega^(42)            = " + Ord(Vector(Ordinal(42)).par))
+  println("omega^(42).isZero     = " + Ord(Vector(Ordinal(42)).par).isZero)
+  println("omega^(42).isFinite   = " + Ord(Vector(Ordinal(42)).par).isFinite)
   println("****************************")
-  println("Ordinal.zero           = " + Ordinal.zero)
-  println("Ordinal.zero.isZero    = " + Ordinal.zero.isZero)
-  println("Ordinal.zero.isFinite  = " + Ordinal.zero.isFinite)
-  println("Ordinal(1)             = " + Ordinal(1))
-  println("Ordinal(1).isZero      = " + Ordinal(1).isZero)
-  println("Ordinal(1).isFinite    = " + Ordinal(1).isFinite)
-  println("Ordinal(42)            = " + Ordinal(42))
-  println("Ordinal(42).isZero     = " + Ordinal(42).isZero)
-  println("Ordinal(42).isFinite   = " + Ordinal(42).isFinite)
-  println("Ordinal(-42)           = " + Ordinal(-42))
-  println("Ordinal(-42).isZero    = " + Ordinal(-42).isZero)
-  println("Ordinal(-42).isFinite  = " + Ordinal(-42).isFinite)
-  println("Ordinal.omega          = " + Ordinal.omega)
-  println("Ordinal.omega.isZero   = " + Ordinal.omega.isZero)
-  println("Ordinal.omega.isFinite = " + Ordinal.omega.isFinite)
+  logg("omegaStack(-42)")(omegaStack(-42))
+  logg("omegaStack(0)")(omegaStack(0))
+  logg("omegaStack(1)")(omegaStack(1))
+  logg("omegaStack(2)")(omegaStack(2))
+  logg("omegaStack(3)")(omegaStack(3))
+  logg("omegaStack(42)")(omegaStack(42))
   println("****************************")
-  println("***** Work in Progress *****")
-  println("Ordinal(0).isSmaller(Ordinal(0)) = " + Ordinal(0).isSmaller(Ordinal(0)))
-  println("Ordinal(0).isSmaller(Ordinal(1)) = " + Ordinal(0).isSmaller(Ordinal(1)))
-  println("Ordinal(1).isSmaller(Ordinal(0)) = " + Ordinal(1).isSmaller(Ordinal(0)))
-  println("Ordinal(1).isSmaller(Ordinal(1)) = " + Ordinal(1).isSmaller(Ordinal(1)))
+  logg(Ordinal(0) + ".isSmaller(" + Ordinal(0) + ")")(Ordinal(0).isSmaller(Ordinal(0)))
+  logg(Ordinal(0) + ".isSmaller(" + Ordinal(42) + ")")(Ordinal(0).isSmaller(Ordinal(42)))
+  logg(Ordinal(42) + ".isSmaller(" + Ordinal(0) + ")")(Ordinal(42).isSmaller(Ordinal(0)))
+  logg(Ordinal(42) + ".isSmaller(" + Ordinal(42) + ")")(Ordinal(42).isSmaller(Ordinal(42)))
+  logg(Ordinal(42) + ".isSmaller(" + omega + ")")(Ordinal(42).isSmaller(omega))
+  logg(omega + ".isSmaller(" + omega + ")")(omega.isSmaller(omega))
   println("****************************")
   println("***** Work in Progress *****")
   println("****************************")
