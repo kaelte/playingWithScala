172fd1480604cb2f3abbe493b962fea2af1ab289
Nikolaus Thiel <klt@fsfe.org>
2019-06-11 17:16:44 +0200

    Implicit ordering for case class Ord

--- src/main/scala/ordinals.scala
+++ src/main/scala/ordinals.scala
@@ -4,18 +4,49 @@ import scala.annotation.tailrec
 import scala.collection.immutable.Vector
 import scala.collection.parallel.immutable._
 import Ordinal._
+import util._
 
-sealed trait Ordinal {
 
-  final def getExpList: ParVector[Ordinal] = this match {case Ord(ords) => ords}
+sealed trait Ordinal
+
+case class Ord(ords: ParVector[Ord]) extends Ordered[Ord]  {
+  final def getExpList: ParVector[Ord] = this match {case Ord(ords) => ords}
 
   final override def toString: String = {
     // a beautiful string
-    if (this.isFinite)
-      this.toBigInt.get.toString
-    else Ordinal.ordVecToString(this.getExpList)
+    if (this.isFinite) this.toBigInt.get.toString else {
+      val expStrs: ParVector[String] = this.getExpList.map(_.toString).map(Ordinal.omegaExpString)
+      expStrs.foldLeft("")((x,y) => if (x.isEmpty) y else x + "+" + y)
+    }
   }
 
+  final def compare(that: Ord) = {
+    if (this.isFinite)
+    // if both ordinals finite use bigInt comparison
+    // else finite is smaller than infinite
+      if (that.isFinite) this.toBigInt.get compare that.toBigInt.get else -1
+    else {
+    // this infinite that finite => 1
+      if (that.isFinite) 1 else {
+        @tailrec
+        def go(aN:ParVector[Ord],bN:ParVector[Ord]):Int = {
+          //go must be called with exponents of normalised ordinals
+          (aN.isEmpty,bN.isEmpty) match {
+            case(true,true) => 0
+            case(true,false) => -1
+            case(false,true) => 1
+            case(false,false) => {
+              val headComp: Int = aN.head.compare(bN.head)
+              if (0==headComp) go(aN.tail,bN.tail) else headComp
+            }
+          }
+        }
+        // we call go with normalised (this,that)
+        go(this.normalise.getExpList,that.normalise.getExpList)
+      }
+    }
+  }
+  
   final def isZero: Boolean = (this == zero)
 
   final def isFinite: Boolean = this.getExpList.forall(_.isZero)
@@ -23,48 +54,44 @@ sealed trait Ordinal {
   final def toBigInt: Option[BigInt] = if (this.isFinite) Some(this.getExpList.length) else None
   // toBigInt returns Some(i) iff this is the finite ordinal i
 
-  final def isSmaller(beta: Ordinal): Boolean = {
-    // returns true iff this is smaller than that
-    if (this.isFinite)
-      if (beta.isFinite)
-        this.toBigInt.get < beta.toBigInt.get
-      else true
-    // work in progress !!!
-      else {
-        println("isSmaller:  !!!! WORK IN PROGRESS: Do not trust the result !!!!")
-        if (beta.isFinite) false else true
-        }
+  final def normalise: Ord = {
+    if (this.isFinite) this else {
+      Ord(ordVecSortedSubVec(this.getExpList).map(_.normalise))
+    }
   }
+
+  final def isNormal: Boolean = {
+    if (this.isFinite) true
+    else {
+      val exps = this.getExpList
+      ordVecIsSorted(exps) && exps.forall(_.isNormal)
+    }
+  }
+
 }
 
-case class Ord(ords: ParVector[Ordinal]) extends Ordinal
+
 
 object Ordinal {
-  final val zero = Ord(Vector().par)
-  final val one = Ord(Vector(zero).par)
-  final val omega = Ord(Vector(one).par)
+  final val zero: Ord = new Ord(Vector().par)
+  final val one:Ord  = Ord(Vector(zero).par)
+  final val omega: Ord = Ord(Vector(one).par)
   final val omegaStr: String = "ω"
+
 
   final def apply(i: Int): Ord = Ord(Vector.fill(i.max(0))(zero).par)
 
-  final def ordVecToString(ords: ParVector[Ordinal]): String = {
-    @tailrec
-    def go(ords: ParVector[Ordinal], str: String): String = {
-      if (ords.isEmpty) str
-      else {
-        val ordsHead: Ordinal = ords.head
-        val headExponent: String = {
-          if (ordsHead == one) ""
-          else {
-            val ordsHeadStr: String = ordsHead.toString
-            if (ordsHeadStr.length == 1) "^" + ordsHeadStr else "^(" + ordsHeadStr + ")"
-          }
-        }
-        if (ords.length == 1) str + omegaStr + headExponent
-        else go(ords.tail, str + omegaStr + headExponent)
-      }
-    }
-    go(ords, "")
+  final def omegaExpString(str:String): String = {
+    if (1==str.length)
+      str match {
+        case "0" => "1"
+        case "1" => Ordinal.omegaStr
+        case _ => omegaStr + "^" + str
+    } else omegaStr + "^(" + str + ")"
+  }
+
+  final def ordVecRemoveInitialSmaller(ords: ParVector[Ord]): ParVector[Ord] = {
+    if (ords.isEmpty) ords else ords.drop(ords.indexOf(ords.max))
   }
 
   final def omegaStack(n: BigInt): Ord = {
@@ -76,11 +103,22 @@ object Ordinal {
     go(n, one)
   }
 
+  final def ordVecIsSorted(ords: ParVector[Ord]): Boolean = ords.sameElements(ords.toVector.sorted)
+
+  final def ordVecSortedSubVec(ords: ParVector[Ord]): ParVector[Ord] = {
+    @tailrec
+    def go(nVec: ParVector[Ord],aVec: ParVector[Ord]): ParVector[Ord] = {
+      if (aVec.isEmpty) nVec else go(nVec :+ aVec.head,ordVecRemoveInitialSmaller(aVec.tail))
+    }
+    go(Vector().par,ordVecRemoveInitialSmaller(ords))
+  }
+
 }
+
+
 
 object ordinalApp extends App {
 
-  import util._
   println("****************************")
   logg("Hellenic letters")("α β γ ω")
   println("****************************")
@@ -105,6 +143,10 @@ object ordinalApp extends App {
   println("omega^(42).isZero     = " + Ord(Vector(Ordinal(42)).par).isZero)
   println("omega^(42).isFinite   = " + Ord(Vector(Ordinal(42)).par).isFinite)
   println("****************************")
+  println("Ordinal(1).toBigInt   = " + Ordinal(1).toBigInt)
+  println("Ordinal(42).toBigInt  = " + Ordinal(42).toBigInt)
+  println("omega.toBigInt        = " + omega.toBigInt)
+  println("****************************")
   logg("omegaStack(-42)")(omegaStack(-42))
   logg("omegaStack(0)")(omegaStack(0))
   logg("omegaStack(1)")(omegaStack(1))
@@ -112,16 +154,28 @@ object ordinalApp extends App {
   logg("omegaStack(3)")(omegaStack(3))
   logg("omegaStack(42)")(omegaStack(42))
   println("****************************")
-  logg(Ordinal(0) + ".isSmaller(" + Ordinal(0) + ")")(Ordinal(0).isSmaller(Ordinal(0)))
-  logg(Ordinal(0) + ".isSmaller(" + Ordinal(42) + ")")(Ordinal(0).isSmaller(Ordinal(42)))
-  logg(Ordinal(42) + ".isSmaller(" + Ordinal(0) + ")")(Ordinal(42).isSmaller(Ordinal(0)))
-  logg(Ordinal(42) + ".isSmaller(" + Ordinal(42) + ")")(Ordinal(42).isSmaller(Ordinal(42)))
-  logg(Ordinal(42) + ".isSmaller(" + omega + ")")(Ordinal(42).isSmaller(omega))
-  logg(omega + ".isSmaller(" + omega + ")")(omega.isSmaller(omega))
+  logg("ordVecSortedSubVec("+Vector(one,Ordinal(42),zero,one).par+")")(ordVecSortedSubVec(Vector(one,Ordinal(42),zero,one).par))
+  logg("ordVecSortedSubVec("+Vector(zero,one,omega).par+")")(ordVecSortedSubVec(Vector(zero,one,omega).par))
+  logg("ordVecSortedSubVec("+Vector(zero,one,omegaStack(2),omegaStack(3),omegaStack(2),omegaStack(1)).par+")")(ordVecSortedSubVec(Vector(zero,one,omegaStack(2),omegaStack(3),omegaStack(2),omegaStack(1)).par))
+  println("****************************")
+  val alpha: Ord = Ord(Vector(one,Ordinal(42),zero,one).par)
+  val beta: Ord = Ord(Vector(zero,one,omegaStack(2),Ord(Vector(omegaStack(2),omegaStack(3)).par),omegaStack(2),omegaStack(1)).par)
+  val gamma: Ord = Ord(Vector(zero,one,omega).par)
+  logg("α")(alpha)
+  logg("β")(beta)
+  logg("γ")(gamma)
+  println("****************************")
+  logg("α.compare(α)")(alpha.compare(alpha))
+  logg("α.compare(β)")(alpha.compare(beta))
+  logg("α.compare(γ)")(alpha.compare(gamma))
+  logg("β.compare(γ)")(beta.compare(gamma))
+  println("****************************")
+  logg("α.normalise")(alpha.normalise)
+  logg("β.normalise")(beta.normalise)
+  logg("γ.normalise")(gamma.normalise)
   println("****************************")
   println("***** Work in Progress *****")
   println("****************************")
-  println("Test : " + -42.max(0))
 }
 
 /// Local Variables:
